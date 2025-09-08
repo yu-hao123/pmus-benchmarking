b1087287acdd5af6c8c7253795cec870a81b560a
yu-hao123 <yuhaowangxia@gmail.com>
2025-09-08 10:55:50 -0300

    feat: create auxiliar method to extract single cycle from acq_table

--- pmus_benchmarking.m
+++ pmus_benchmarking.m
@@ -20,36 +20,14 @@ pmus_estimate = acq_table.pmus_estimate; % PMUS-MAG
 
 [ins_marks, exp_marks] = retrieve_parity_marks(volume * 10);
 fprintf("retrieved %d ins/exp marks from volume parity bit\n", length(ins_marks));
+
 %%
-offset = 60;
+offset = 50;
 idx1 = 9000;
 idx2 = 9001;
-
-interval = (ins_marks(idx1)-offset):(ins_marks(idx2)-offset);
-interval_table = acq_table(interval, :);
-interval_table.pmus = interval_table.pmus;
-interval_table.flow = fir_filter(8, 0.2, 100, interval_table.flow);
-interval_table.pressure = fir_filter(8, 0.2, 100, interval_table.pressure);
-
-plot_dataset(interval_table);
-
-exp_start = exp_marks(idx1) - ins_marks(idx1) + offset;
-
 peep = 5;
-waveforms = table();
-waveforms.time = interval_table.time;
-waveforms.paw = interval_table.pressure - peep;
-waveforms.flow = interval_table.flow;
-waveforms.volume = interval_table.volume;
-waveforms.pmus = interval_table.pmus;
-waveforms.pmus_mag = interval_table.pmus_estimate;
-insexp = ones(length(waveforms.paw), 1);
-for i=1:length(waveforms.paw)
-    if i >= exp_start
-        insexp(i) = 0;
-    end
-end
-waveforms.insexp = insexp;
+waveforms = extract_cycle(acq_table, ins_marks(idx1), ins_marks(idx1 + 1), exp_marks(idx1), peep);
+plot_dataset(acq_table(ins_marks(idx1) - offset:ins_marks(idx2) - offset, :));
 
 %% pmus miqp estimation
 [waveforms_true, waveforms_hat, params_true, params_hat] = pmus_miqp(waveforms, false, true, 0);
@@ -65,19 +43,19 @@ pmus_true_miqp = waveforms_true.pmus;
 %paw_est = waveforms_hat.paw;
 
 %% plot
-[f, t, linkplot] = plot_dataset(interval_table);
+[f, t, linkplot] = plot_dataset(waveforms);
 
-plot(linkplot(3), interval_table.time - interval_table.time(1), pmus_optimized);
+plot(linkplot(3), waveforms.time - waveforms.time(1), pmus_optimized);
 
 resistance = params_true.resistance; % cmH2O / (L * s)
 compliance = 1000 / params_true.elastance; % cmH2O / mL
 pmus_recalculated = waveforms.paw - resistance / 60 * waveforms.flow - waveforms.volume / (compliance);
-plot(linkplot(1), interval_table.time - interval_table.time(1), paw_est + peep);
+plot(linkplot(1), waveforms.time - waveforms.time(1), paw_est);
 legend(linkplot(1), 'paw ASL', 'paw MIQP est');
 
-plot(linkplot(3), interval_table.time - interval_table.time(1), pmus_recalculated);
-plot(linkplot(3), interval_table.time - interval_table.time(1), waveforms.pmus_mag);
-plot(linkplot(3), interval_table.time - interval_table.time(1), waveforms.insexp);
+plot(linkplot(3), waveforms.time - waveforms.time(1), pmus_recalculated);
+plot(linkplot(3), waveforms.time - waveforms.time(1), waveforms.pmus_mag);
+plot(linkplot(3), waveforms.time - waveforms.time(1), waveforms.insexp);
 legend('pmus ASL', 'pmus MIQP', 'pmus LS recalculated', 'pmus MAG', 'expiration switch');
 
 fprintf("true parameters - \n");
@@ -98,4 +76,40 @@ function J = cost(resistance, elastance, paw, flow, volume, pmus)
     paw_hat = pmus + resistance .* flow + (volume * elastance);
     resid = paw - paw_hat;
     J = sum(resid.^2);
+end
+
+function waveforms = extract_cycle(acq_table, ins_mark, next_ins_mark, exp_mark, peep, offset)
+arguments
+    acq_table
+    ins_mark
+    next_ins_mark
+    exp_mark
+    peep
+    offset = 50
+end
+
+interval = (ins_mark-offset):(next_ins_mark-offset);
+interval_table = acq_table(interval, :);
+interval_table.pmus = interval_table.pmus;
+interval_table.flow = fir_filter(8, 0.2, 100, interval_table.flow);
+interval_table.pressure = fir_filter(8, 0.2, 100, interval_table.pressure);
+
+exp_start = exp_mark - ins_mark + offset;
+
+waveforms = table();
+waveforms.time = interval_table.time;
+waveforms.paw = interval_table.pressure - peep;
+waveforms.pressure = waveforms.paw; % not ideal
+waveforms.flow = interval_table.flow;
+waveforms.volume = interval_table.volume;
+waveforms.pmus = interval_table.pmus;
+waveforms.pmus_mag = interval_table.pmus_estimate;
+insexp = ones(length(waveforms.paw), 1);
+for i=1:length(waveforms.paw)
+    if i >= exp_start
+        insexp(i) = 0;
+    end
+end
+waveforms.insexp = insexp;
+
 end
